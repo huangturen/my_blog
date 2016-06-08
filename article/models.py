ff5b5b2d73748407e1d4705f8e905fb1c91caeee
huangturen <huangturen@sjtu.edu.cn>
2016-06-08 16:57:51 -0700

    simple structure is ready

--- article/models.py
+++ article/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 class Article(models.Model):
@@ -9,7 +9,10 @@ class Article(models.Model):
     category = models.CharField(max_length = 50, blank = True)
     date_time = models.DateTimeField(auto_now_add = True)
     content = models.TextField(blank = True, null = True)
-  
+    
+    def get_absolute_url(self):
+	path = reverse('detail',kwargs={'id':self.id})
+	return "http://localhost:9000%s" %path 
     def __unicode__(self):
         return self.title
 
